6c91d2fbb4ebec06a49b00b708efc47d9945d0b4
KernelCode <kernelcod3@gmail.com>
2014-12-31 03:26:08 +0300

    Update CreateAsciiBox.java

--- CreateAsciiBox.java
+++ CreateAsciiBox.java
@@ -14,50 +14,54 @@ public class CreateAsciiBox {
   * AbdullahAltahery@gmail.com
   * facebook.com/kernelcode
   */
-  static String spaces(int num){
-  	if(num<0)
-  		num*=-1;
-  	char [] f= new  char [num];
-  	for(int i=0;i<num;i++){
-  		f[i]=' ';
-  	}
+
+  /* return a number of Spaces */
+	static String spaces(int num){
+		if( num < 0 )
+			num *= -1 ;
+		char [] f = new  char [num];
+		for( int i = 0 ; i < num ; i++ ){
+			f[i] = ' ';
+		}
   	
-  	return new String(f);
-  }
+		return new String(f);
+	}
   
-  static String setDash(int num){
-  	if(num<0)
-  		num*=-1;
-  	char [] f= new  char [num];
-  	for(int i=0;i<num;i++){
-  		f[i]='-';
-  	}
-  	
-  	return new String(f);
-  }
-  static void printBox(int x,int y){
+  /* return a number of Dashes */
+	static String setDash( int num ){
+		if( num < 0 )
+			num *= -1 ;
+		char [] f= new  char [num];
+		for( int i = 0 ; i < num ; i ++ ){
+			f[i] = '-';
+		}
+		return new String(f);
+	}
   
-  	System.out.println("#"+setDash(x*2)+"#");
-  	int f=0;
-  	int i=0;
-  	for(f=0;f<y;f++){
-  	System.out.print("|"+spaces(f)+"\\");
+  /* Print the Box  */
+	static void printBox( int x,int y ){
+		
+		System.out.println( "#" + setDash( x * 2 ) + "#" );
+		int f=0;
+		
+
+		for( f = 0 ; f < y ; f++ ) {
+			System.out.print( "|" + spaces( f ) + "\\" );
+			System.out.println( spaces( ( x * 2 ) ) + "\\");
+		}
+		
+		System.out.print( "#" );
+		System.out.println( spaces( f ) + "#" + setDash( x * 2 ) + "#" );
+		
+		for( f = 1 ; f < y + 1; f ++ ) {
+			System.out.println( spaces( f ) + "\\" + spaces( y - f ) + "|" + spaces( x * 2 ) + "|" );
+		}
+		
+		System.out.println( spaces( f ) + "#" + setDash( x * 2 ) + "#" );
+		  
+	}
   
-  		System.out.println(spaces((x*2))+"\\");
-  	}
-  	System.out.print("#");
-  	System.out.println(spaces(f)+"#"+setDash(x*2)+"#");
-  	
-  	for(f=1;f<y+1;f++){
-  		
-  		
-  		System.out.println(spaces(f)+"\\"+spaces(y-f)+"|"+spaces(x*2)+"|");
-  	}
-  	System.out.println(spaces(f)+"#"+setDash(x*2)+"#");
-  
-  }
-  
-  public static void main(String args[]){
-      printBox(9,3);
-  }
+	public static void main(String args[]){
+		printBox( 9 , 3 );
+	}
 }
